d7ba01dc91642d1b0b39d13489cc0d9e1dd9a04b
Alex <cupokoffi@gmail.com>
2022-02-19 22:04:30 -0500

    Duncker

--- gr8/src/Components/Ass3.jsx
+++ gr8/src/Components/Ass3.jsx
@@ -1,4 +1,5 @@
 import React from "react"; 
+import Duncker from "../Images/Image.jpg"; 
 
 function Features2(){
     return(
@@ -8,8 +9,18 @@ function Features2(){
             <hr className="divider" /> 
             <h2 id="title">Sentiment Analysis</h2>
 
-            <h3 id="subject">Header </h3>
-            <p id="description">Paragraph</p>
+            <h3 id="subject">Statement-Restatement Technique</h3>
+            <p id="description">Problem:<br /> There is an urgency to keep up with information in an organized and fast way.<br /> 
+            <br />
+            Constraints:<br /> It is perhaps impossible for software in its current state to reach a level at which it can interpret emotions 
+in the same way that the human brain can. The current methods of sentiment analysis can find trouble with more complicated 
+vocabulary, such as compound words, which can create a false meaning in the eyes of a software that is designed to determine 
+emotions based on words alone. <br /> 
+<br />
+Goals:<br /> Effectively generate user perspectives based on their use of words and information. </p>
+
+<h3 id="subject">Duncker Diagram</h3>
+<img src={Duncker} className="img" />
                 
             </div> 
         </div> 
