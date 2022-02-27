462236a47f535e417b81cf03fc9ffba7c1d2ae56
Carlitos Rodriguez <78374556+CarlRod2001@users.noreply.github.com>
2022-02-26 22:11:36 -0500

    Update Ass4.jsx
    Added Paths to Avoid

--- gr8/src/Components/Ass4.jsx
+++ gr8/src/Components/Ass4.jsx
@@ -12,29 +12,29 @@ function Features3(){
             <h3 id="subject">Solution:</h3>
             <h id="topic">Proposed Solution: "Pathosense," The Software that Understands Emotions</h>
             <p id="description-list">
-                •Compiler that can be used to create and edit code over time
+                • Compiler that can be used to create and edit code over time
                 </p>
             <br /> 
 
             <h id="topic">Requirements:</h>
             <p id="description-list">
-                •Compiler that can be used to create and edit code over time<br />
-                •Database where information can be effetively read from<br />
-                •Efficient algorithm that can allow for fast web scraping<br />
-                •Jupyter or IDE of equivalent prevalence 
+                • Compiler that can be used to create and edit code over time<br />
+                • Database where information can be effetively read from<br />
+                • Efficient algorithm that can allow for fast web scraping<br />
+                • Jupyter or IDE of equivalent prevalence 
                 </p>
 
             <br /> 
 
             <h3 id="subject">Design Approach:</h3>
             <p id="description-list">
-                •Begin by becoming more familiar with Python IDE's<br /> 
-                •Create a test software that can read text from a specific source<br /> 
-                •Fabricate a database where information can be stored and read from<br /> 
-                •Connect the database with a software that can be used to submit information<br /> 
-                •Work with Python to implement web scraping<br /> 
-                •Implement an algorithm that analyzes text submitted to the database<br /> 
-                •Program a method that depicts emotions based on language used in database submissions 
+                • Begin by becoming more familiar with Python IDE's<br /> 
+                • Create a test software that can read text from a specific source<br /> 
+                • Fabricate a database where information can be stored and read from<br /> 
+                • Connect the database with a software that can be used to submit information<br /> 
+                • Work with Python to implement web scraping<br /> 
+                • Implement an algorithm that analyzes text submitted to the database<br /> 
+                • Program a method that depicts emotions based on language used in database submissions 
                 </p>
             <br /> 
 
@@ -44,19 +44,16 @@ function Features3(){
                 • Enable users to change and customize the software in anyway that they want to find the best results  <br />
                 • Efficiently produce outputs based on huge datasets that would not slow down the users machine alot <br />
                 • Correctly output results based on real world data using our designed algorithms <br />
-                
-            
-            
                 </p>
             <br /> 
 
             <h3 id="subject">Attributes:</h3>
             <p id="description-list">
-            •Reliably display the correct information that the customer requestes from the software <br />
-            •Make the software easy to scale to make it possible for hundreds or more users to use it at the same time, while still being efficient  <br />
-            •The software should have good portability to be used on any operating system and device<br />
-            •The users should be able to resue the software in many differt ways and applications <br />
-            •Reliably display the correct information that the customer requests from the software <br />
+            • Reliably display the correct information that the customer requestes from the software <br />
+            • Make the software easy to scale to make it possible for hundreds or more users to use it at the same time, while still being efficient  <br />
+            • The software should have good portability to be used on any operating system and device<br />
+            • The users should be able to resue the software in many differt ways and applications <br />
+            • Reliably display the correct information that the customer requests from the software <br />
 
             
                 </p>
@@ -64,7 +61,14 @@ function Features3(){
 
             <h3 id="subject">Paths to Avoid:</h3>
             <p id="description-list">
-                •
+            • Manually analyzing the given data as the program should be automatic.
+            • Creating an app version as the program should only work in the web browser.
+            • Neglecting to create a satisfying minimum viable product.
+            • Cluttering the webpage with intrusive advertisements.
+            • Not updating the code frequently enough.
+            • Underestimating how much it would cost to develop the program and host the webpage.
+            • Ignoring customer feedback and criticism.
+
                 </p>
             <br /> 
            
