9620f0710b2b4280e39b8934e75cf0600c36a5c9
Alex <cupokoffi@gmail.com>
2022-02-28 20:00:43 -0500

    Logo

--- gr8/src/Components/Navbar.jsx
+++ gr8/src/Components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react"; 
 import { Link } from "react-scroll"; 
+import img from "../Images/Pathosense-Bar.png"; 
 
 function Navbar(){
     return(
         <>
             <nav>
                 <a href='http://cupokoffi8.github.io/the-website' className='logo'>
-                    <h1 className="test">Group 8</h1>  
+                    <img className="test" src={img} /> 
                 </a>
                 <input className='menu-btn' type='checkbox' id='menu-btn'/> 
                 <label className='menu-icon' for='menu-btn'>
