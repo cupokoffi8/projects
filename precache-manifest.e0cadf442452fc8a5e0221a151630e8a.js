60f1f223344fc6e7706768d1014fdcdf81330e84
NikolaCiric7117 <nciric@stevens.edu>
2022-03-09 10:10:31 -0500

    Updates

---
+++ precache-manifest.e0cadf442452fc8a5e0221a151630e8a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "90b5a7238d75525609c8",
+    "url": "/static/css/main.f9ed1ea5.chunk.css"
+  },
+  {
+    "revision": "90b5a7238d75525609c8",
+    "url": "/static/js/main.90b5a723.chunk.js"
+  },
+  {
+    "revision": "5bd4f02185e8c2b65f7e",
+    "url": "/static/js/1.5bd4f021.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a5d1d67ed7c0e44a07ffe2115a9905f",
+    "url": "/static/media/Pathosense.3a5d1d67.jpg"
+  },
+  {
+    "revision": "27e577757670998e8e6bd1ccdf4c19d1",
+    "url": "/static/media/Pathosense-Bar.27e57775.png"
+  },
+  {
+    "revision": "c38bb44cb1776d8682a4cdbb0628c67d",
+    "url": "/static/media/Image.c38bb44c.jpg"
+  },
+  {
+    "revision": "a1c5c25e6473f34ebc3a7470b53fb8b1",
+    "url": "/static/media/what.a1c5c25e.jpeg"
+  },
+  {
+    "revision": "7794202c91aa6de7534d1c74eccbefae",
+    "url": "/static/media/when.7794202c.jpeg"
+  },
+  {
+    "revision": "336065041f9f4744fbefe0b375982b08",
+    "url": "/static/media/where.33606504.jpg"
+  },
+  {
+    "revision": "e06ffb5d1f15335d81f095dae1bdb154",
+    "url": "/static/media/why.e06ffb5d.jpg"
+  },
+  {
+    "revision": "18caa8d1a6c4328b0484c19a8d6448fd",
+    "url": "/static/media/who.18caa8d1.jpg"
+  },
+  {
+    "revision": "5f9226d761f227e191c2b03c64df55e5",
+    "url": "/static/media/how.5f9226d7.jpg"
+  },
+  {
+    "revision": "61a04e867cd68a586a15b0a86a58a9b0",
+    "url": "/static/media/uh.61a04e86.jpeg"
+  },
+  {
+    "revision": "1ad72896f01a2bbc0115267ae5bf6820",
+    "url": "/index.html"
+  }
+];
